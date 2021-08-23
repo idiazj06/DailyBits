23828f2102de26af5a51f74fef3b2baa9f0b9276
Ilan Diaz <idiazjimenez06@gmail.com>
2021-08-23 16:28:33 -0500

    Pagina estadisticas - responsive 90%

--- scripts/scriptPractica.js
+++ scripts/scriptPractica.js
@@ -1,26 +1,14 @@
-const halfCircles = document.querySelectorAll('.half-circle')
-const halfCircleTop = document.querySelector('.half-circle-top')
-const progressBarCircle = document.querySelector('.progressbar-circle')
+let checkbox = document.getElementById('input')
 
-document.addEventListener('scroll', () => {
-    const pageViewportHeight = window.innerHeight
-    const pageHeight = document.documentElement.scrollHeight
-    const scrolledPortion = window.pageYOffset
-    
-    const scrolledPortionDegree = (scrolledPortion / (pageHeight - pageViewportHeight)) * 360
 
-    const scrolledPortionPercent = Math.floor(scrolledPortionDegree / 360 * 100)
 
-    progressBarCircle.textContent = `${scrolledPortionPercent}%`
-
-    halfCircles.forEach(el => {
-        el.style.transform = `rotate(${scrolledPortionDegree}deg)`
-
-        if(scrolledPortionDegree >= 180) {
-            halfCircles[0].style.transform = 'rotate(180deg)'
-            halfCircleTop.style.opacity = '0'
-        } else {
-            halfCircleTop.style.opacity = '1'
-        }
-    })
-})
+checkbox.addEventListener("change", validaCheckbox, false);
+function validaCheckbox()
+{
+  var checked = checkbox.checked;
+  if(checked){
+    console.log('checkbox1 esta seleccionado');
+  }else{
+      console.log('no lo esta')
+  }
+}
