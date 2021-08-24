102af0a2362164219cd46ce28e6acd25196ab9dd
Ilan Diaz <idiazjimenez06@gmail.com>
2021-08-23 22:43:21 -0500

    UPDATE funcionalidad

--- scripts/scriptDesarrollo.js
+++ scripts/scriptDesarrollo.js
@@ -1,27 +1,28 @@
-let botonComprobar = document.querySelector('.btn-comprobar');
-let botonContinuar = document.querySelector('.btn-continuar');
-let alerta = document.querySelector('.alerta')
+// let botonComprobar = document.querySelector('.btn-comprobar');
+// let botonContinuar = document.querySelector('.btn-continuar');
+// let alerta = document.querySelector('.alerta')
 
 
-botonComprobar.addEventListener('click', e => {
-    agregarAlerta(e);
-    e.preventDefault();
+// botonComprobar.addEventListener('click', e => {
+//     agregarAlerta(e);
+//     e.preventDefault();
+//     console.log('click')
     
-})
+// })
 
 
 
-function agregarAlerta(e) {
-    alerta.classList.add('alertaSeleccionada')
-}
-botonContinuar.addEventListener('click', e => {
-    devolverAlerta(e);
-    e.preventDefault();
+// function agregarAlerta(e) {
     
-})
+// }
+// botonContinuar.addEventListener('click', e => {
+//     devolverAlerta(e);
+//     e.preventDefault();
+    
+// })
 
 
 
-function devolverAlerta(e) {
-    alerta.classList.remove('alertaSeleccionada')
-}
+// function devolverAlerta(e) {
+//     alerta.classList.remove('alertaSeleccionada')
+// }
--- scripts/scriptPractica.js
+++ scripts/scriptPractica.js
@@ -1,14 +1,59 @@
 let checkbox = document.getElementById('input1')
-
+let botonComprobar = document.querySelector('.btn-comprobar');
+let textoBotonComprobar = document.getElementById('texto-boton')
+let botonContinuar = document.querySelector('.btn-continuar');
+let alerta = document.querySelector('.alerta')
+let imgCheckbox = document.getElementById('img') 
 
 
 checkbox.addEventListener("change", validaCheckbox, false);
-function validaCheckbox()
-{
+
+function validaCheckbox() {
   var checked = checkbox.checked;
-  if(checked){
+  if (checked) {
     console.log('checkbox1 esta seleccionado');
-  }else{
-      console.log('no lo esta')
+    botonComprobar.classList.add('activado')
+    textoBotonComprobar.classList.add('activado')
+    
+  } else {
+    console.log('no lo esta')
   }
-} 
+}
+
+botonComprobar.addEventListener('click', e => {
+    agregarAlerta(e);
+    e.preventDefault();
+    console.log(checkbox.checked)
+    
+    
+})
+
+
+
+function agregarAlerta(e) {
+  // alerta.classList.add('alertaSeleccionada')
+  if (e.target.classList.contains('activado')) {
+    console.log('si')
+    alerta.classList.add('alertaSeleccionada')
+  }else{
+    return
+  }
+}
+
+
+botonContinuar.addEventListener('click', e => {
+    devolverAlerta(e);
+    e.preventDefault();
+})
+
+
+
+function devolverAlerta(e) {
+    alerta.classList.remove('alertaSeleccionada')
+    botonComprobar.classList.remove('activado')
+    textoBotonComprobar.classList.remove('activado')
+    checkbox.checked = false
+    imgCheckbox.checked = false
+    
+}
+
