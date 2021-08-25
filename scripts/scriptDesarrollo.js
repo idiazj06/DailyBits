8b656f030c555d24986dab8381c23f75de9ec861
Ilan Diaz <idiazjimenez06@gmail.com>
2021-08-25 12:43:27 -0500

    nueva rama

--- scripts/scriptDesarrollo.js
+++ scripts/scriptDesarrollo.js
@@ -1,28 +1,63 @@
-// let botonComprobar = document.querySelector('.btn-comprobar');
-// let botonContinuar = document.querySelector('.btn-continuar');
-// let alerta = document.querySelector('.alerta')
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData();
+
+})
+
+const fetchData = async () => {
+    try {
+        const res = await fetch('http://localhost:4000/html/')
+        const data = await res.json()
+        console.log(data)
+        pintarCards(data)
+    } catch (error) {
+        console.log(error)
+
+    }
+}
 
 
-// botonComprobar.addEventListener('click', e => {
-//     agregarAlerta(e);
-//     e.preventDefault();
-//     console.log('click')
-    
-// })
+const pintarCards = data => {
+    console.log(data)
+
+    let imagen = data.pregunta1.imagen
+    let pregunta = data.pregunta1.pregunta
+    let opcion1 = data.pregunta1.opcion1
+    let opcion2 = data.pregunta1.opcion2
+    let opcion3 = data.pregunta1.opcion3
+    let input1 = document.getElementById('input1')
+    let input2 = document.getElementById('input2')
+    let input3 = document.getElementById('input3')
+    console.log(opcion1.valor, opcion2.valor, opcion3.valor)
 
 
+    document.getElementById('character-joven').setAttribute('src', imagen)
+    document.getElementById('texto-principal').textContent = pregunta
+    document.getElementById('opcion1').textContent = opcion1.dato
+    document.getElementById('opcion2').textContent = opcion2.dato
+    document.getElementById('opcion3').textContent = opcion3.dato
+    input1.value = opcion1.valor
+    input2.value = opcion2.valor
+    input3.value = opcion3.valor
+    console.log(input1.value, input2.value, input3.value)
 
-// function agregarAlerta(e) {
-    
-// }
-// botonContinuar.addEventListener('click', e => {
-//     devolverAlerta(e);
-//     e.preventDefault();
-    
-// })
+    if (input1.value === "true") {
+        document.getElementById('input1').classList = 'verdadero'
+
+    } else {
+        document.getElementById('input1').classList = 'falso'
+    }
+    if (input2.value === "true") {
+        document.getElementById('input2').classList = 'verdadero'
+
+    } else {
+        document.getElementById('input2').classList = 'falso'
+    }
+    if (input3.value === "true") {
+        document.getElementById('input3').classList = 'verdadero'
+
+    } else {
+        document.getElementById('input3').classList = 'falso'
+    }
 
 
-
-// function devolverAlerta(e) {
-//     alerta.classList.remove('alertaSeleccionada')
-// }
+}
--- scripts/scriptPractica.js
+++ scripts/scriptPractica.js
@@ -1,59 +1,129 @@
-let checkbox = document.getElementById('input1')
+let checkbox1 = document.getElementById('input1')
+let checkbox2 = document.getElementById('input2')
+let checkbox3 = document.getElementById('input3')
 let botonComprobar = document.querySelector('.btn-comprobar');
 let textoBotonComprobar = document.getElementById('texto-boton')
 let botonContinuar = document.querySelector('.btn-continuar');
 let alerta = document.querySelector('.alerta')
-let imgCheckbox = document.getElementById('img') 
+let imgCheckbox1 = document.getElementById('img1')
+let imgCheckbox2 = document.getElementById('img2')
+let imgCheckbox3 = document.getElementById('img3')
 
+checkbox1.addEventListener("change", validaCheckbox, false);
+checkbox2.addEventListener("change", validaCheckbox2, false);
+checkbox3.addEventListener("change", validaCheckbox3, false);
 
-checkbox.addEventListener("change", validaCheckbox, false);
+console.log(checkbox2)
 
 function validaCheckbox() {
-  var checked = checkbox.checked;
-  if (checked) {
+  var checked = checkbox1.checked;
+  if (checked && checkbox1.value === 'true') {
     console.log('checkbox1 esta seleccionado');
-    botonComprobar.classList.add('activado')
-    textoBotonComprobar.classList.add('activado')
-    
+    botonComprobar.classList.add('verdadero')
+    textoBotonComprobar.classList.add('verdadero')
+    document.getElementById('labelInput1').classList = 'inputVerdadero'
+  } else if (checked && checkbox1.value === 'false') {
+    console.log('checkbox1 esta seleccionado');
+    botonComprobar.classList.add('falso')
+    textoBotonComprobar.classList.add('falso')
+    document.getElementById('labelInput1').classList = 'inputFalso'
   } else {
     console.log('no lo esta')
+    botonComprobar.classList.remove('verdadero')
+    textoBotonComprobar.classList.remove('verdadero')
+    document.getElementById('labelInput1').classList = 'input'
   }
 }
 
+function validaCheckbox2() {
+  var checked = checkbox2.checked;
+  if (checked && checkbox2.value === 'true') {
+    console.log('checkbox2 esta seleccionado');
+    botonComprobar.classList.add('verdadero')
+    textoBotonComprobar.classList.add('verdadero')
+    document.getElementById('labelInput2').classList = 'inputVerdadero'
+  } else if (checked && checkbox2.value === 'false') {
+    console.log('checkbox2 esta seleccionado');
+    botonComprobar.classList.add('falso')
+    textoBotonComprobar.classList.add('falso')
+    document.getElementById('labelInput2').classList = 'inputFalso'
+  } else {
+    console.log('no lo esta')
+    botonComprobar.classList.remove('verdadero')
+    textoBotonComprobar.classList.remove('verdadero')
+    botonComprobar.classList.remove('falso')
+    textoBotonComprobar.classList.remove('falso')
+    document.getElementById('labelInput2').classList = 'input'
+  }
+}
+function validaCheckbox3() {
+  var checked = checkbox3.checked;
+  if (checked && checkbox3.value === 'true') {
+    console.log('checkbox3 esta seleccionado');
+    botonComprobar.classList.add('verdadero')
+    textoBotonComprobar.classList.add('verdadero')
+    document.getElementById('labelInput3').classList = 'inputVerdadero'
+  } else if (checked && checkbox3.value === 'false') {
+    console.log('checkbox3 esta seleccionado');
+    botonComprobar.classList.add('falso')
+    textoBotonComprobar.classList.add('falso')
+    document.getElementById('labelInput3').classList = 'inputFalso'
+  } else {
+    console.log('no lo esta')
+    botonComprobar.classList.remove('verdadero')
+    textoBotonComprobar.classList.remove('verdadero')
+    document.getElementById('labelInput3').classList = 'input'
+
+  }
+}
+
+
+
 botonComprobar.addEventListener('click', e => {
-    agregarAlerta(e);
-    e.preventDefault();
-    console.log(checkbox.checked)
-    
-    
+  agregarAlerta(e);
+  e.preventDefault();
+  
 })
-
-
 
 function agregarAlerta(e) {
   // alerta.classList.add('alertaSeleccionada')
-  if (e.target.classList.contains('activado')) {
+  if (e.target.classList.contains('verdadero')) {
     console.log('si')
-    alerta.classList.add('alertaSeleccionada')
-  }else{
+    alerta.classList.add('alertaVerdadera')
+  }else if(e.target.classList.contains('falso')){
+    alerta.classList.remove('alert-success')
+    alerta.classList.add('alert-danger')
+    alerta.classList.add('alertaFalsa')
+    document.getElementById('texto-alert').textContent = 'La respuesta correcta es'
+    document.getElementById('texto-sec-alert').textContent = 'main'
+    document.querySelector('.btn-continuar').classList.add('btn-danger')
+    document.querySelector('.btn-continuar').style.backgroundColor = 'none'
+    console.log(alerta)
+  } 
+  else {
     return
   }
 }
 
-
 botonContinuar.addEventListener('click', e => {
-    devolverAlerta(e);
-    e.preventDefault();
+  devolverAlerta(e);
+  e.preventDefault();
+  console.log(e.target)
 })
 
-
-
 function devolverAlerta(e) {
-    alerta.classList.remove('alertaSeleccionada')
-    botonComprobar.classList.remove('activado')
-    textoBotonComprobar.classList.remove('activado')
-    checkbox.checked = false
-    imgCheckbox.checked = false
-    
-}
+  alerta.classList.remove('alertaVerdadera')
+  alerta.classList.remove('alertaFalsa')
+  botonComprobar.classList.remove('verdadero')
+  textoBotonComprobar.classList.remove('verdadero')
+  checkbox1.checked = false
+  checkbox2.checked = false
+  checkbox3.checked = false
+  imgCheckbox1.checked = false
+  imgCheckbox2.checked = false
+  imgCheckbox3.checked = false
+  document.getElementById('labelInput1').classList = 'input'
+  document.getElementById('labelInput2').classList = 'input'
+  document.getElementById('labelInput3').classList = 'input'
 
+}
